29b2ceabb2d7d0ec8ca6b2b22bfdee8a03625380
adewunmi <shonibareadewunmicomfort@gmail.com>
2021-08-07 14:22:07 +0100

    display api scores

--- src/listeners.js
+++ src/listeners.js
@@ -5,3 +5,15 @@ export const refreshBtnListener = async (e) => {
     displayList(result)
 }
 
+export const displayList = (result) => {
+    const ul = document.querySelector('#score');
+    ul.innerHTML = '';
+    result.forEach(({user, score}) => {
+        const li = document.createElement('li');
+        li.textContent = `${user}: ${score}`;
+        ul.appendChild(li);
+    } )
+}
+
+
+
